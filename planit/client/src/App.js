0e14d5dccced93403a5ce7980a973a686db685d3
Brook Trout <walkerradez@gmail.com>
2018-07-03 20:11:16 -0600

    Added a navbar....I think? Super derpy without any CSS or frameworks. Also got the react routing setup properly.

--- planit/client/src/App.js
+++ planit/client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import ErrPage from './pages/ErrPage';
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Fragment>
+          <NavBar />
           <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route component={ErrPage} />
