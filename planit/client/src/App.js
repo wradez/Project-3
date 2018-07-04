a9e1ba425769d476eeec4f985f6b9ab1be43403d
Brook Trout <walkerradez@gmail.com>
2018-07-04 12:43:30 -0600

    will it work this time?

--- planit/client/src/App.js
+++ planit/client/src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/HomePage/HomePage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import ErrPage from './pages/ErrPage/ErrPage';
 import NavBar from './components/NavBar/NavBar';
-
+//stupid stuff
 class App extends Component {
   render() {
     return (
