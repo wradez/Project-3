bfdf7d357444ee8dc2449aa145e5b03bfd099d97
thamma513 <thamma513@gmail.com>
2018-07-06 11:21:49 -0600

    sign-in not working

--- planit/controllers/planitContollers.js
+++ planit/controllers/planitContollers.js
@@ -3,32 +3,32 @@ const db = require("../models")
 module.exports = {
 
     all: function(req,res){
-        db.//database name
+        db.planItdb
         .find(req.query)
         .sort({})
-        .then(db//database name => res.json(db//datbase name))
+        .then(db.planItdb => res.json(db.planItdb))
         .catch(err => res.status(422).json)
     },
         
     create: function(req,res){
-        db.//the database name goes here
+        db.planItdb
         .create(req.body)
-        .then(db//database name)
+        .then(db.planItdb)
         .catcher(err => res.status(422).json(err));
     },
 
     update: function(req,res){
-        db.//the database name goes here
+        db.planItdb
         .findOneAndUpdate({_id:req.params.id}, req.body)
-        .then(db//database name => res.json(db//database name goes here))
+        .then(db.planItdb => res.json(db.planItdb))
         .catcher(err => res.status(422).json(err));
     },
 
     remove: function(req,res){
-        db.//database name goes here
+        db.planItdb
         .findById({_id: req.params.id})
-        .then(db//database name => db//database name.remove())
-        .then(db//database name => res.json(db//database name))
+        .then(db.planItdb => db.planItdb.remove())
+        .then(db.planItdb => res.json(db.planItdb))
         .catch(err => res.status(422).json)
     }
 };
