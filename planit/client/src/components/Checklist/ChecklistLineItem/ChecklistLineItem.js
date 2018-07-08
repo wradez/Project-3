69ee064abc08db5bc1cec7945a35bdce4deb6eeb
Ashley Winnop <arwinnop@gmail.com>
2018-07-08 07:30:00 -0600

    styling

--- planit/client/src/components/Checklist/ChecklistLineItem/ChecklistLineItem.js
+++ planit/client/src/components/Checklist/ChecklistLineItem/ChecklistLineItem.js
@@ -1,28 +1,63 @@
-import React from 'react';
-import './ChecklistLineItem.css';
+import React, { Component, Fragment } from 'react';
+import styled from 'styled-components';
 
-const ChecklistLineItem = props => {
+const ItemDiv = styled.div`
+    display: inline-flex;
+`;
 
-    const checklistItems = [
-        {
-            id: 1,
-            name: 'Fire Wood',
-            price: 5
-        }, 
-        {
-            id: 2,
-            name: 'Lighter Fluid',
-            price: 4
-        }, 
-        {
-            id: 3,
-            name: 'Charcoal',
-            price: 6
-        }
-    ];
+const LineItem = styled.p`s
+    margin-left: 1em;
+    justify-content: flex-start;
+    font-family: 'Roboto', sans-serif;
+`;
 
-    return (checklistItems.map(lineItem => ( <p className='lineItem' key={lineItem.id}>{lineItem.name} | Price: ${lineItem.price}</p>) )
-    )
+const ItemPrice = styled.p`
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 1em;
+    font-family: 'Roboto', sans-serif;
+`;
+
+const HR = styled.hr`
+    opacity: 0.9%;
+`;
+
+class ChecklistLineItem extends Component {
+
+    render () {
+        return (
+            <Fragment>
+                <ItemDiv>
+                    <LineItem>
+                        Item #1
+                    </LineItem>
+                    <ItemPrice>
+                        $ XX
+                    </ItemPrice>
+                </ItemDiv>
+                <HR />
+                <ItemDiv>
+                    <LineItem>
+                        Item #2
+                    </LineItem>
+                    <ItemPrice>
+                        $ XX
+                    </ItemPrice>
+                </ItemDiv>
+                <HR />
+                <ItemDiv>
+                    <LineItem>
+                        Item #3
+                    </LineItem>
+                    <ItemPrice>
+                        $ XX
+                    </ItemPrice>
+                </ItemDiv>
+                <HR />
+            </Fragment>
+        )
+    }
+        
 }
 
 export default ChecklistLineItem;
