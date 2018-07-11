ea5828e310c1a4a8338a475a508407a3e55f076e
Ashley Winnop <arwinnop@gmail.com>
2018-07-11 09:11:31 -0600

    icons

--- planit/client/src/components/Checklist/ChecklistLineItem/ChecklistLineItem.js
+++ planit/client/src/components/Checklist/ChecklistLineItem/ChecklistLineItem.js
@@ -2,9 +2,9 @@ import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import check from '../../../img/baseline-check_circle_outline-24px.svg'
 
-const ItemDiv = styled.div`
+const ItemDiv = styled.li`
     display: inline-flex;
-    float: left;
+    margin-left: 10px;
 `;
 
 const LineItem = styled.p`
@@ -17,28 +17,26 @@ const ItemSymbol = styled.p`
     font-family: 'Roboto', sans-serif;
     margin-top: .2em;
     margin-bottom: .2em;
-`;
-
-const HR = styled.hr`
-    opacity: 0.9%;
+    float: left;
 `;
 
 const ItemIcon = styled.div`
     width: 25px;
     height: 25px;
+    float: left;
 `;
 
-
 const ItemInput = styled.input`
-	padding: .25em;
+    float: left;
+	padding: .2em;
 	color: black;
 	background: #ffffff;
 	border: none;
 	border-radius: 4px;
-	width: 150px;
+	width: 100%;
 	margin-top: .25em;
 	margin-bottom: .25em;
-	margin-left: .25em;
+	margin-left: .1em;
 	margin-right: .25em;
 	font-family: 'Roboto', sans-serif;
 `;
@@ -76,6 +74,7 @@ class ChecklistLineItem extends Component {
     render () {
         return (
             <Fragment>
+                <ul>
                 <ItemDiv>
                     <ItemIcon>
                         <img src={check} alt="checkmark"/>
@@ -105,6 +104,37 @@ class ChecklistLineItem extends Component {
                         placeholder="Cost"
                     />  
                 </ItemDiv>
+                
+                <ItemDiv>
+                    <ItemIcon>
+                        <img src={check} alt="checkmark"/>
+                    </ItemIcon>
+                    <ItemInput 
+                        type="text"
+                        id="item"
+                        name="item"
+                        placeholder="Item"
+                    />
+                    <ItemSymbol>
+                        #
+                    </ItemSymbol>
+                    <QuantityInput
+                        type="text"
+                        id="quantity"
+                        name="quantity"
+                        placeholder="Quantity"
+                    />
+                    <ItemSymbol>
+                        $
+                    </ItemSymbol>
+                    <CostInput 
+                        type="text"
+                        id="cost"
+                        name="cost"
+                        placeholder="Cost"
+                    />  
+                </ItemDiv>
+                </ul>
                 {/* <HR /> */}
                 {/* <ItemDiv>
                     <LineItem>
